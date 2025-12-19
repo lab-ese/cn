c1693117db17e87557b7610982fa9f9492fd38e4
lab-ese <starktonymk85@gmail.com>
2025-12-20 02:26:36 +0530

    Simplfied

--- tcp_server.py
+++ tcp_server.py
@@ -1,27 +1,16 @@
-# tcp_server.py
+#tcp_server.py
 import socket
 
-# 1. Create socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# 2. Bind IP and port
-server_socket.bind(("127.0.0.1", 8080))
-
-# 3. Listen for client
-server_socket.listen(1)
+s = socket.socket()
+s.bind(("127.0.0.1", 8080))
+s.listen(1)
 print("TCP Server waiting...")
 
-# 4. Accept client
-conn, addr = server_socket.accept()
+c, addr = s.accept()
 print("Connected to:", addr)
+msg = c.recv(1024).decode()
+print("Client: ", msg)
 
-# 5. Receive data
-data = conn.recv(1024).decode()
-print("Client:", data)
-
-# 6. Send response
-conn.send("Hello from TCP Server".encode())
-
-# 7. Close connection
-conn.close()
-server_socket.close()
+print("Sending response...")
+c.send("Hello".encode())
+c.close()
