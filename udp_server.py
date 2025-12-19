007f01860c43087bf707cded9e50180860136839
lab-ese <starktonymk85@gmail.com>
2025-12-20 02:27:18 +0530

    Simplfied

--- udp_server.py
+++ udp_server.py
@@ -1,18 +1,12 @@
-# udp_server.py
+#udp_server.py
 import socket
 
-# 1. Create socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# 2. Bind IP and port
-server_socket.bind(("127.0.0.1", 8080))
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(("127.0.0.1", 8080))
 print("UDP Server waiting...")
 
-# 3. Receive message
-data, addr = server_socket.recvfrom(1024)
-print("Client:", data.decode())
+data, addr = s.recvfrom(1024)
+print("Client: ", data.decode())
 
-# 4. Send response
-server_socket.sendto("Hello from UDP Server".encode(), addr)
-
-server_socket.close()
+print("Sending response...")
+s.sendto("Hello".encode(), addr)
